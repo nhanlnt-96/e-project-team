c05c9c6f8f958eba99f89d276b6f5e83887eed79
nhanlnt <nhanlnt@hotmail.com>
2023-05-01 00:25:25 +0700

    feat: created update and remove account

--- api/src/main/java/com/main/api/controller/UserController.java
+++ api/src/main/java/com/main/api/controller/UserController.java
@@ -1,11 +1,13 @@
 package com.main.api.controller;
 
+import com.main.api.configs.EmailService;
 import com.main.api.constant.Constant;
 import com.main.api.dao.RoleRepository;
 import com.main.api.dao.UserRepository;
 import com.main.api.dto.UserDto;
 import com.main.api.entity.Role;
 import com.main.api.entity.User;
+import com.main.api.mailTemplate.MailTemplate;
 import com.main.api.model.UserModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -14,6 +16,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.security.RolesAllowed;
+import javax.mail.MessagingException;
 import javax.persistence.NoResultException;
 import javax.validation.Valid;
 import java.util.Objects;
@@ -26,12 +30,14 @@ public class UserController {
     final private UserRepository userRepository;
     final private PasswordEncoder passwordEncoder;
     final private RoleRepository roleRepository;
+    final private EmailService emailService;
 
     @Autowired
-    public UserController(UserRepository userRepository, PasswordEncoder passwordEncoder, RoleRepository roleRepository) {
+    public UserController(UserRepository userRepository, PasswordEncoder passwordEncoder, RoleRepository roleRepository, EmailService emailService) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
         this.roleRepository = roleRepository;
+        this.emailService = emailService;
     }
 
     @PostMapping("/register")
@@ -53,12 +59,67 @@ public class UserController {
             if (role != null) {
                 boolean assignRoleResponse = assignRoleForUser(user.getUserId(), role.getRoleId());
                 if (assignRoleResponse) {
+                    try {
+                        emailService.sendSimpleMessage(userSaveResponse.getEmail(), "Register Successful", MailTemplate.WelcomeMail(userSaveResponse.getFullName()), true);
+                    } catch (MessagingException messagingException) {
+                        System.out.println(messagingException.getMessage());
+                    }
                     return new ResponseEntity<>(new UserDto(userSaveResponse.getUserId(), userSaveResponse.getAddressDetail(), userSaveResponse.getPhoneNumber(), userSaveResponse.getEmail(), userSaveResponse.getFullName()), HttpStatus.CREATED);
                 }
             }
         }
 
         throw new NoResultException("Register failed.");
+    }
+
+    @PostMapping("/create-new-account")
+    @Transactional(rollbackFor = Exception.class)
+    @RolesAllowed("ROLE_ADMIN")
+    public ResponseEntity<UserDto> createNewAccount(@Valid @RequestBody UserModel.CreateNewAccount userData) {
+        User checkEmailExist = userRepository.findByEmail(userData.getEmail()).orElse(null);
+        User checkPhoneNumberExist = userRepository.findByPhoneNumber(userData.getPhoneNumber()).orElse(null);
+        if (checkEmailExist != null) throw new NoResultException("Email already exist.");
+        if (checkPhoneNumberExist != null) throw new NoResultException("Phone number already exist.");
+
+        var rawPassword = userData.getPassword();
+        var encodedPassword = passwordEncoder.encode(rawPassword);
+        User user = new User(userData.getAddressDetail(), userData.getPhoneNumber(), userData.getEmail(), encodedPassword, userData.getFullName());
+        User userSaveResponse = userRepository.save(user);
+        if (userSaveResponse.getUserId() != null) {
+            boolean assignRoleResponse = assignRoleForUser(user.getUserId(), userData.getRoleId() != null ? userData.getRoleId() : roleRepository.findByRoleName(defaultRoleName).getRoleId());
+            if (assignRoleResponse) {
+                try {
+                    emailService.sendSimpleMessage(userSaveResponse.getEmail(), "Register Successful", MailTemplate.WelcomeMail(userSaveResponse.getFullName()), true);
+                } catch (MessagingException messagingException) {
+                    System.out.println(messagingException.getMessage());
+                }
+                return new ResponseEntity<>(new UserDto(userSaveResponse.getUserId(), userSaveResponse.getAddressDetail(), userSaveResponse.getPhoneNumber(), userSaveResponse.getEmail(), userSaveResponse.getFullName()), HttpStatus.CREATED);
+            }
+        }
+
+        throw new NoResultException("Create account failed.");
+    }
+
+    @PutMapping("/update-account")
+    @Transactional(rollbackFor = Exception.class)
+    public ResponseEntity<UserDto> updateAccount(@Valid @RequestBody UserModel.UpdateAccount updateAccount) {
+        User checkUserExist = userRepository.findById(updateAccount.getUserId()).orElseThrow(() -> new NoResultException("Account does not exist."));
+        if (updateAccount.getAddressDetail() != null) checkUserExist.setAddressDetail(updateAccount.getAddressDetail());
+        if (updateAccount.getPhoneNumber() != null) checkUserExist.setPhoneNumber(updateAccount.getPhoneNumber());
+        if (updateAccount.getFullName() != null) checkUserExist.setFullName(updateAccount.getFullName());
+
+        User updateUserResponse = userRepository.saveAndFlush(checkUserExist);
+
+        return new ResponseEntity<>(new UserDto(updateUserResponse.getUserId(), updateUserResponse.getAddressDetail(), updateUserResponse.getPhoneNumber(), updateUserResponse.getEmail(), updateUserResponse.getFullName()), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/remove-account/{userId}")
+    @Transactional(rollbackFor = Exception.class)
+    @RolesAllowed("ROLE_ADMIN")
+    public ResponseEntity<String> removeAccount(@Valid @PathVariable("userId") Long userId) {
+        User checkUserExist = userRepository.findById(userId).orElseThrow(() -> new NoResultException("Account does not exist."));
+        userRepository.delete(checkUserExist);
+        return new ResponseEntity<>("Removed account.", HttpStatus.OK);
     }
 
     private Role getRoleDataByName(String roleName) {
--- api/src/main/java/com/main/api/model/UserModel.java
+++ api/src/main/java/com/main/api/model/UserModel.java
@@ -111,4 +111,128 @@ public class UserModel {
             this.password = password;
         }
     }
+
+    public static class CreateNewAccount {
+        private Long roleId;
+        private String addressDetail;
+        @NotNull(message = "phoneNumber can not be null")
+        private String phoneNumber;
+        @NotNull(message = "email can not be null")
+        @Email(message = "email invalid")
+        private String email;
+        @NotNull(message = "password can not be null")
+        private String password;
+        @NotNull(message = "fullName can not be null")
+        private String fullName;
+
+        public CreateNewAccount() {
+        }
+
+        public CreateNewAccount(Long roleId, String addressDetail, String phoneNumber, String email, String password, String fullName) {
+            this.roleId = roleId;
+            this.addressDetail = addressDetail;
+            this.phoneNumber = phoneNumber;
+            this.email = email;
+            this.password = password;
+            this.fullName = fullName;
+        }
+
+        public Long getRoleId() {
+            return roleId;
+        }
+
+        public void setRoleId(Long roleId) {
+            this.roleId = roleId;
+        }
+
+        public String getAddressDetail() {
+            return addressDetail;
+        }
+
+        public void setAddressDetail(String addressDetail) {
+            this.addressDetail = addressDetail;
+        }
+
+        public String getPhoneNumber() {
+            return phoneNumber;
+        }
+
+        public void setPhoneNumber(String phoneNumber) {
+            this.phoneNumber = phoneNumber;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+
+        public String getFullName() {
+            return fullName;
+        }
+
+        public void setFullName(String fullName) {
+            this.fullName = fullName;
+        }
+    }
+
+    public static class UpdateAccount {
+        @NotNull(message = "User id can not be null.")
+        private Long userId;
+        private String addressDetail;
+        private String phoneNumber;
+        private String fullName;
+
+        public UpdateAccount() {
+        }
+
+        public UpdateAccount(Long userId, String addressDetail, String phoneNumber, String fullName) {
+            this.userId = userId;
+            this.addressDetail = addressDetail;
+            this.phoneNumber = phoneNumber;
+            this.fullName = fullName;
+        }
+
+        public Long getUserId() {
+            return userId;
+        }
+
+        public void setUserId(Long userId) {
+            this.userId = userId;
+        }
+
+        public String getAddressDetail() {
+            return addressDetail;
+        }
+
+        public void setAddressDetail(String addressDetail) {
+            this.addressDetail = addressDetail;
+        }
+
+        public String getPhoneNumber() {
+            return phoneNumber;
+        }
+
+        public void setPhoneNumber(String phoneNumber) {
+            this.phoneNumber = phoneNumber;
+        }
+
+        public String getFullName() {
+            return fullName;
+        }
+
+        public void setFullName(String fullName) {
+            this.fullName = fullName;
+        }
+    }
 }
